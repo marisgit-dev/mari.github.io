e29ae0f3c8704c1ef3d2fe3b0cc25a71c1bb9f97
marisgit-dev <183960467+mariazi-ui@users.noreply.github.com>
2025-11-25 15:06:29 -0500

    Editing Navbar and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+import '../css/Footer.css';
+
+export default function Footer() {
+
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import "../css/NavBar.css";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <div>
+                <h2>Home</h2>
+            </div>
+            <div>
+                <h2>About</h2>
+                <h2>Resume</h2>
+                <h2>Project</h2>
+            </div>
+            <div>
+                <h2>Contact</h2>
+            </div>
+        </nav>
+    );
+}
